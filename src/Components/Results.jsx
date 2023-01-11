5c10870576daff69d43fd3f21913af2cf8125498
nwils000 <nwils000@gmail.com>
2023-01-10 21:04:09 -0500

    Added back button in results component

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import Result from "./Result";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import backButton from "../Photos/backButton.png";
 
 export default function Results() {
   const location = useLocation();
   let results = location.state.results;
 
   return (
-    <div className="results__container">
-      {results.slice(0, 5).map((result) => {
-        return <Result key={uuidv4()} result={result} />;
-      })}
-    </div>
+    <>
+      <Link to="/">
+        <img className="back-button" src={backButton} alt="" />
+      </Link>
+      <div className="results__container">
+        {results.slice(0, 5).map((result) => {
+          return <Result key={uuidv4()} result={result} />;
+        })}
+      </div>
+    </>
   );
 }
